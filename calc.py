5f1b2e8a1fd345efaba257a4e45e8bcacc2334c7
kostech111 <kostech111@gmail.com>
2026-04-02 10:36:03 +0900

    파이썬 연습 파일 추가

--- calc.py
+++ calc.py
@@ -1,6 +1,3 @@
-﻿num1 = int(input('첫 번째 숫자: '))
-num2 = int(input('두 번째 숫자: '))
+﻿num1 = 10
+num2 = 5
 print('더하기:', num1 + num2)
-print('빼기:', num1 - num2)
-print('곱하기:', num1 * num2)
-print('나누기:', num1 / num2)
